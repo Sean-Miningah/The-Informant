8030fc24afdca9d3fac74efe1c1c57ecaad1e6ba
Sean Miningah <miningahsean@gmail.com>
2021-09-03 00:20:42 +0300

    index page debugging(not working prototype)

--- news/views.py
+++ news/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound 
 from .models import Story
 from users.models import Comments
 
 
+
 # Create your views here.
 def index(request):
+    stories = Story.objects.all().order_by('-Datetime')
+    nation_stories = Story.objects.order_by('-Datetime').filter(
+        source ="Nation Media").exclude(img_url=None)[:4]
+    # pd_stories = Story.objects.filter(
+    #     source="People Daily").order_by('-Datetime')[:4]
+    # pd_images = pd_stories.img_url
+    # pd_titles = pd_stories.title
+    # nation_stories = Story.objects.filter(source="Nation Media").order_by('-Datetime')[:4]
+    # nation_images = nation_stories.img_url 
+    # nation_titles = nation_storis.title
 
+    context = {
+        nation_stories : 'nation_stories',
+    }
+    # print(type(pd_stories))
+    # print(dir(pd_stories))
+    # print(dir(stories))
+    print(nation_stories)
+    # for story in stories:
+    #     print(story)
     # return HttpResponse('<h1>Page Found</h1>')
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 
 def comments(request):
